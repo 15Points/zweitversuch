cce66cf8bc89cc86f923285d9654a9638bcecad5
15Points <s_hoehne21@stud.hwr-berlin.de>
2023-10-10 15:35:21 +0200

    update forms for login and registration forms

--- forms.py
+++ forms.py
@@ -13,3 +13,29 @@ class TodoForm(FlaskForm):
     description = StringField(validators=[InputRequired()])
     list_id = SelectField(coerce=int, choices=[], validate_choice=False)
     submit = SubmitField('Update')
+
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+#from db import db
+#
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(
+        min = 2, max = 20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(
+        min = 4, max = 20)], render_kw={"placeholder": "Password"})
+    submit = SubmitField("Register")
+#
+#    def validate_username(self, username):
+#        existing_user_username = db.User.query.filter_by(
+#            username = username.data).first()
+#        if existing_user_username:
+#            raise ValidationError(
+#                "This username has already been taken. Please choose a different one."
+#            )
+#        
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(
+        min = 2, max = 20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(
+        min = 4, max = 20)], render_kw={"placeholder": "Password"})
+    submit = SubmitField("Login")
